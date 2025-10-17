777497aa866e36e620629aa0daa436b668aa0cab
CalebSutherland <sutherlandcaleb12@gmail.com>
2025-10-17 15:17:06 -0700

    Add github profile card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,40 @@
 import { useState } from "react";
 import "./App.css";
 
+function ProfileCard({ profile }) {
+  const username = profile.login;
+  const avatar_url = profile.avatar_url;
+  const bio = profile.bio ?? "No bio";
+  const followers = profile.followers;
+  const following = profile.following;
+  const url = profile.html_url;
+  const public_repos = profile.public_repos;
+
+  return (
+    <div className="profile-card">
+      <img src={avatar_url} alt={`${username}'s avatar`} className="avatar" />
+      <p className="profile-title">{profile.login}</p>
+      <div className="profile-followers">
+        <div>
+          <p className="follower-value">{followers}</p>
+          <p className="follower-label">followers</p>
+        </div>
+        <div>
+          <p className="follower-value">{following}</p>
+          <p className="follower-label">following</p>
+        </div>
+      </div>
+      <p className="bio">{bio}</p>
+      <p>
+        <b>Public Repos:</b> {public_repos}
+      </p>
+      <a href={url} target="_blank">
+        Profile
+      </a>
+    </div>
+  );
+}
+
 function App() {
   const [userInput, setUserInput] = useState("");
   const [profile, setProfile] = useState(null);
@@ -45,6 +79,7 @@ function App() {
       </div>
       {loading && <p>loading...</p>}
       {error && <p style={{ color: "red" }}>Error: {error}</p>}
+      {profile && <ProfileCard profile={profile} />}
     </div>
   );
 }
